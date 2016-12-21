6d31ee8668eb19eebc5288e38fa26c1646a4ad09
Marie Treschow <treschow.marie@gmail.com>
2016-12-21 17:52:59 +0100

    working on the game

--- custom.js
+++ custom.js
@@ -10,13 +10,25 @@ var delay = 0;
 
 for(var i = 0; i < count; i++) {
    var duck = $('<img src="ducky.png">');
-
    duck.css({
            position: 'absolute',
            left: 0,
            top: '25%',
            width: '150px'
-      })
+      });
+
+      duck.appendTo('#duck');
+      dealy = delay + 800;
+      duck.hide().duck(delay).show(1).animate({'left':screenWidth}, {
+         duration: 5000,
+         easing: "linear",
+         complete: function() {
+             $(this).remove()
+         }
+      });
+      duck.click(function () {
+            $(this).remove()
+      });
    }
 }
 
