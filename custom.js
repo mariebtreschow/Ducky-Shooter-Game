e91be508b8127f6085f357cd2a2c15254d388c20
Marie Treschow <treschow.marie@gmail.com>
2016-12-21 14:36:17 +0100

    setting up the game

---
+++ custom.js
@@ -0,0 +1,22 @@
+
+//variable screenWidth
+var screenWidth = $(document).width();
+
+
+//function to create the ducks
+
+function createDucks(count) {
+var delay = 0;
+
+for(var i = 0; i < count; i++) {
+   var duck = $('<img src="ducky.png">')
+   }
+}
+
+
+
+//running the function
+
+$(document).ready(function() {
+   createDucks(20);
+});
